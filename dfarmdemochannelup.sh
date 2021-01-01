4084f5ecf9e672bb8e48cb72fcbc2b1db685dbc9
abhisamant7 <abhisamanta29@gmail.com>
2021-01-01 11:00:13 +0530

    correction

--- dfarmdemochannelup.sh
+++ dfarmdemochannelup.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 docker exec cli peer channel create -o orderer.dfarmorderer.com:7050 -c demodfarmchannel -f ./channel-artifacts/demodfarmchannel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dfarmorderer.com/orderers/orderer.dfarmorderer.com/msp/tlscacerts/tlsca.dfarmorderer.com-cert.pem
-=======
-docker exec cli peer channel create -o orderer.dfarmorderer.com:7050 -c demodfarmchannel -f ./channel-artifact/demodfarmchannel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dfarmorderer.com/orderers/orderer.dfarmorderer.com/msp/tlscacerts/tlsca.dfarmorderer.com-cert.pem
->>>>>>> a60e2929d9786facceb39ae7e6564936b80a151c
 sleep 5
 
 docker exec cli peer channel join -b demodfarmchannel.block
@@ -11,12 +7,6 @@ docker exec -e CORE_PEER_ADDRESS=peer0.dfarmadmin.com:7051 -e CORE_PEER_TLS_ROOT
 
 docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/dfarmdemo.com/users/Admin@dfarmdemo.com/msp -e CORE_PEER_ADDRESS=peer0.dfarmdemo.com:9051 -e CORE_PEER_LOCALMSPID="DfarmdemoMSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/dfarmdemo.com/peers/peer0.dfarmdemo.com/tls/ca.crt cli peer channel join -b demodfarmchannel.block
 
-<<<<<<< HEAD
 docker exec cli peer channel update -o orderer.dfarmorderer.com:7050 -c demodfarmchannel -f ./channel-artifacts/DfarmadminMSPanchors1.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dfarmorderer.com/orderers/orderer.dfarmorderer.com/msp/tlscacerts/tlsca.dfarmorderer.com-cert.pem
 
 docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/dfarmdemo.com/users/Admin@dfarmdemo.com/msp -e CORE_PEER_ADDRESS=peer0.dfarmdemo.com:9051 -e CORE_PEER_LOCALMSPID="DfarmdemoMSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/dfarmdemo.com/peers/peer0.dfarmdemo.com/tls/ca.crt cli peer channel update -o orderer.dfarmorderer.com:7050 -c demodfarmchannel -f ./channel-artifacts/DfarmdemoMSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dfarmorderer.com/orderers/orderer.dfarmorderer.com/msp/tlscacerts/tlsca.dfarmorderer.com-cert.pem
-=======
-docker exec cli peer channel update -o orderer.dfarmorderer.com:7050 -c demodfarmchannel -f ./channel-artifact/DfarmadminMSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dfarmorderer.com/orderers/orderer.dfarmorderer.com/msp/tlscacerts/tlsca.dfarmorderer.com-cert.pem
-
-docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/dfarmdemo.com/users/Admin@dfarmdemo.com/msp -e CORE_PEER_ADDRESS=peer0.dfarmdemo.com:9051 -e CORE_PEER_LOCALMSPID="DfarmdemoMSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/dfarmdemo.com/peers/peer0.dfarmdemo.com/tls/ca.crt cli peer channel update -o orderer.dfarmorderer.com:7050 -c demodfarmchannel -f ./channel-artifact/DfarmdemoMSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/dfarmorderer.com/orderers/orderer.dfarmorderer.com/msp/tlscacerts/tlsca.dfarmorderer.com-cert.pem
->>>>>>> a60e2929d9786facceb39ae7e6564936b80a151c
